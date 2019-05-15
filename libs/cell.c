89c66d3b0c20eb1c4258f21b559784a1773c7e7b
Bastien <bastien.tagliaro@gmail.com>
2019-05-15 20:46:29 +0200

    Structures + fonctions utilitaires

--- libs/cell.c
+++ libs/cell.c
@@ -2,14 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include "memoryUtils.h"
 
-void initializeCell(cell_t *cell, char *name, double value) {
-    sprintf(cell->name, "%s", name);
-    cell->value = value;
+void initializeCell(cell_t *cell, char *name, variable_type type) {
+    if ((cell->name = strdup(name)) == NULL) {
+        fprintf(stderr, "Erreur: mémoire insuffisante\n");
+        exit(EXIT_FAILURE);
+    }
+
+    cell->type = type;
     cell->prev = NULL;
     cell->next = NULL;
 }
 
 void destroyCell(cell_t *cell) {
+    free(cell->name);
     free(cell);
+}
+
+char* getVariableType(variable_type type) {
+    switch (type) {
+        case TYPE_BOOLEAN:
+        case TYPE_INT:
+            return "int";
+    }
+}
+
+variable_type charToVariableType(char *type) {
+    variable_type rtype;
+
+    if (strcmp(type, "entier") == 0) {
+        rtype = TYPE_INT;
+    }
+
+    else if (strcmp(type, "booléen") == 0) {
+
+    }
 }
--- libs/cell.h
+++ libs/cell.h
@@ -1,13 +1,31 @@
 #ifndef TP3_CELLULE_H
 #define TP3_CELLULE_H
 
+typedef enum {
+    TYPE_BOOLEAN = 0,
+    TYPE_INT,
+    TYPE_VOID
+} variable_type;
+
+typedef struct {
+    variable_type type;
+    char *name;
+} argument_t;
+
+typedef struct {
+    int nbArguments;
+    argument_t **arguments;
+} function_t;
+
 typedef struct cell {
-    char name[256];
-    double value;
+    char *name;
+    variable_type type;
     struct cell *prev, *next;
 } cell_t;
 
-void initializeCell(cell_t*, char*, double);
+void initializeCell(cell_t*, char*, variable_type);
 void destroyCell(cell_t*);
+char* getVariableType(variable_type type);
+variable_type charToVariableType(char *type);
 
 #endif
